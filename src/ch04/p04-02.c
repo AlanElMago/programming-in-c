2c2bb4881428ed5030209a3f2d1358acfa6e6bb9
Alan Franco <alanfrancob97@gmail.com>
2021-12-27 15:48:16 -0600

    Added src/ch04/p04-02.c and src/ch04/p04-03.c

---
+++ src/ch04/p04-02.c
@@ -0,0 +1,16 @@
+// Illustrate the use of various arithmetic operators
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 100;
+    int b = 2;
+    int c = 25;
+    int d = 4;
+    int result;
+
+    result = a - b;
+    printf ("")    
+}
+
