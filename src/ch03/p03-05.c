69cf57440418e593342cb3b96f3daf52caa03d1a
Alan Franco <alanfrancob97@gmail.com>
2021-12-27 04:43:11 -0600

    Refactored src/ch03/p03-05.c

--- src/ch03/p03-05.c
+++ src/ch03/p03-05.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-    int value1, value2, value3;
+    int value1, value2, sum;
 
     value1 = 50;
     value2 = 25;
-    value3 = value1 + value2;
-    printf("The sum of %i and %i is %i\n", value1, value2, value3);
+    sum = value1 + value2;
+    printf("The sum of %i and %i is %i\n", value1, value2, sum);
 
     return 0;
 }
