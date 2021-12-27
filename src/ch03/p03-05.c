85f0cefe476aaaa8e27d47cd9e0dbb43e0697e7e
Alan Franco <alanfrancob97@gmail.com>
2021-12-27 04:39:42 -0600

    Added src/ch03/p03-05.c

---
+++ src/ch03/p03-05.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int value1, value2, value3;
+
+    value1 = 50;
+    value2 = 25;
+    value3 = value1 + value2;
+    printf("The sum of %i and %i is %i\n", value1, value2, value3);
+
+    return 0;
+}
+
