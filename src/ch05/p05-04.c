4666729efe4f34755152dfe7adedeed88c618e57
Alan Franco <alanfrancob97@gmail.com>
2021-12-28 02:38:15 -0600

    Added src/ch05/p05-04.c and src/ch05/p05-05.c

---
+++ src/ch05/p05-04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, number, triangularNumber;
+
+    printf("What triangular number do you want? ");
+    scanf("%i", &number);
+
+    triangularNumber = 0;
+
+    for (n = 1; n <= number; n = n + 1)
+        triangularNumber += n;
+
+    printf("The triangular number %i is %i\n", number, triangularNumber);
+
+    return 0;
+}
+
